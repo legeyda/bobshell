347f082756e51356af1e1bbd74dc83451a7591ef
Alexander Legeyda <alex@legeyda.com>
2025-02-19 16:03:02 +0300

    fix event

--- event/template.sh
+++ event/template.sh
@@ -4,5 +4,22 @@ shelduck import ../base.sh
 
 
 bobshell_event_template() {
-	bobshell_putvar "${1}_template" "$2"
+	if bobshell_isset_2 "$@"; then
+		if bobshell_isset "${1}_template"; then
+			_bobshell_event_template=$(bobshell_getvar "${1}_template")
+			if [ "$_bobshell_event_template" == "$2" ]; then
+				unset _bobshell_event_template
+				return
+			fi
+			unset _bobshell_event_template
+		fi
+		bobshell_putvar "${1}_template" "$2"
+	else
+		if bobshell_isset "${1}_template"; then
+			unset "${1}_template"
+		else
+			return
+		fi
+	fi
+	unset -f "$1"
 }
