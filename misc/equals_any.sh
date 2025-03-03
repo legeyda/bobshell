a4e260a4d831aca2a903dcb1f979520aebbd5d34
Alexander Legeyda <alex@legeyda.com>
2025-03-03 21:04:55 +0300

    bobshell_equals_any

---
+++ misc/equals_any.sh
@@ -0,0 +1,23 @@
+
+
+shelduck import ../base.sh
+
+bobshell_equals_any() {
+	if ! bobshell_isset_3 "$@"; then
+		if [ "$1" = "$2" ]; then
+			return 0
+		else
+			return 1
+		fi
+	fi
+	bobshell_equals_value="$1"
+	shift
+	while bobshell_isset_1 "$@"; do
+		if [ "$bobshell_equals_value" = "$1" ];  then
+			return
+		fi
+		shift
+	done
+	unset bobshell_equals_value
+	return 1
+}
