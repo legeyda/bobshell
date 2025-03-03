9a9228719ae1fc6737b17685c8f26329631896fc
Alexander Legeyda <alex@legeyda.com>
2025-03-03 21:11:52 +0300

    bobshell_eqvar

---
+++ misc/eqvar.sh
@@ -0,0 +1,25 @@
+
+shelduck import ../base.sh
+
+# fun: bobshell_equals [VALUE1 [VALUE2]]
+# txt: return ok if both arguments are defined and the same
+bobshell_eqvar() {
+	if bobshell_isset "$1"; then
+		if ! bobshell_isset "$2"; then
+			return 1
+		fi
+	else
+		if bobshell_isset "$2"; then
+			return 1
+		else
+			return 0
+		fi 
+	fi
+
+	if eval "test \"\$$1\" = \"\$$2\""; then
+		return 0
+	else
+		return 1
+	fi
+
+}
