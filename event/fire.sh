7d3ba96ad33cf2e0e5e09cca9f1055db4f393feb
Alexander Legeyda <alex@legeyda.com>
2025-03-03 22:04:16 +0300

    fix event

--- event/fire.sh
+++ event/fire.sh
@@ -4,22 +4,31 @@ shelduck import ../string.sh
 shelduck import ../misc/defun.sh
 
 bobshell_event_fire() {
+	if bobshell_command_available "$1"; then
+		"$@"
+		return
+	fi
+
 	_bobshell_event_fire__name="$1"
 	shift
-	if ! bobshell_command_available "$_bobshell_event_fire__name"; then
-		_bobshell_event_fire__code=$(bobshell_getvar "$_bobshell_event_fire__name")
-		if bobshell_isset "${_bobshell_event_fire__name}_template"; then
-			_bobshell_event_fire__template=$(bobshell_getvar "${_bobshell_event_fire__name}_template")
-			_bobshell_event_fire__code=$(bobshell_replace "$_bobshell_event_fire__template" '{}' "$_bobshell_event_fire__code")
-			unset _bobshell_event_fire__template
-		fi
-		if [ -n "$_bobshell_event_fire__code" ]; then
-			bobshell_defun "$_bobshell_event_fire__name" "$_bobshell_event_fire__code"
-		else
-			bobshell_defun "$_bobshell_event_fire__name" true
-		fi
-		unset _bobshell_event_fire__code
+	_bobshell_event_fire__code=$(bobshell_getvar "$_bobshell_event_fire__name")
+	if bobshell_isset "${_bobshell_event_fire__name}_template"; then
+		_bobshell_event_fire__template=$(bobshell_getvar "${_bobshell_event_fire__name}_template")
+		_bobshell_event_fire__code=$(bobshell_replace "$_bobshell_event_fire__template" '{}' "$_bobshell_event_fire__code")
+		unset _bobshell_event_fire__template
 	fi
-	"$_bobshell_event_fire__name" "$@"
-	unset _bobshell_event_fire__name
+	if [ -n "$_bobshell_event_fire__code" ]; then
+		bobshell_defun "$_bobshell_event_fire__name" "$_bobshell_event_fire__code"
+	else
+		bobshell_defun "$_bobshell_event_fire__name" true
+	fi
+	unset _bobshell_event_fire__code
+
+
+	if "$_bobshell_event_fire__name" "$@"; then
+		unset _bobshell_event_fire__name
+	else
+		unset _bobshell_event_fire__name
+		return 1
+	fi
 }
--- event/listen.sh
+++ event/listen.sh
@@ -5,11 +5,15 @@ shelduck import ../string.sh
 bobshell_event_listen() {
 	_bobshell_event_listen__name="$1"
 	shift
-	_bobshell_event_listen__code=$(bobshell_getvar "$_bobshell_event_listen__name" '')
-	if [ -n "$_bobshell_event_listen__code" ] && [ -n "$*" ]; then
-		_bobshell_event_listen__code="$_bobshell_event_listen__code$bobshell_newline$bobshell_newline"
+	if [ -z "${*:-}" ]; then
+		return
 	fi
-	_bobshell_event_listen__code="$_bobshell_event_listen__code$*"
+	_bobshell_event_listen__code=$(bobshell_getvar "$_bobshell_event_listen__name" '')
+	if [ -n "$_bobshell_event_listen__code" ]; then
+		_bobshell_event_listen__code="$_bobshell_event_listen__code$bobshell_newline$bobshell_newline$*"
+	else
+		_bobshell_event_listen__code="$*"
+	fi
 	bobshell_putvar "$_bobshell_event_listen__name" "$_bobshell_event_listen__code"
 	unset _bobshell_event_listen__code
 	unset -f "$_bobshell_event_listen__name"
