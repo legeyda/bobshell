83c6653f0ee68dd96fe81e170bd9f1b2ea8cbfc2
Alexander Legeyda <alex@legeyda.com>
2025-03-06 16:02:14 +0300

    refactor bobtest

---
+++ bobtest.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env shelduck_run
+set -eu
+
+shelduck import base.sh
+shelduck import string.sh
+
+bobtest() {
+	stdout_file=$(mktemp)
+	stderr_file=$(mktemp)
+	file_separator=
+	for x in "$@"; do
+		printf %s "$file_separator"
+		bobtest_file "$x"
+		file_separator="$bobshell_newline"
+	done
+}
+
+# fun: bobtest_test_file SCRIPT[:FUNCTION]
+bobtest_file() {
+	_bobtest_file="$1"
+	unset _bobtest_file__function
+	bobshell_split_first "$_bobtest_file" : _bobtest_file _bobtest_file__function || true
+
+	_bobtest_file_real_path=$(realpath "$_bobtest_file")
+	printf 'file %s:\n' "$_bobtest_file"
+	if [ -n "${_bobtest_file__function:-}" ]; then
+		_bobtest_file__function "$_bobtest_file_real_path" "$_bobtest_file__function"
+	else
+		all__bobtest_file__functions=$(sed -n --regexp-extended 's/^( *function)? *(test_\w+) *\( *\) *\{ *$/\2/pg' "$_bobtest_file_real_path")
+		for _bobtest_file__function in $all__bobtest_file__functions; do
+			bobtest_file_function "$_bobtest_file_real_path" "$_bobtest_file__function"
+		done
+	fi
+}
+
+# fun: bobtest_file_function SCRIPT FUNCTION
+bobtest_file_function() {
+	bobshell_basic_regex_match "$2" '^[A-Za-z_][A-Za-z_0-9]*$' || bobshell_die "wrong function name: $2"
+
+	printf '  function %s... ' "$2"
+	if ! sh -c "shelduck_run 'val:shelduck import \"file://$1\"; set -x; $2'" > "$stdout_file" 2> "$stderr_file"; then
+		printf 'failure\n\n'
+		printf '\n\nSTDOUT WAS:\n%s\n'
+		cat "$stdout_file"
+		
+		printf '\n\nSTDERR WAS:\n%s\n'
+		cat "$stderr_file"
+		
+		exit
+	fi
+	printf ' ok\n'
+}
+
