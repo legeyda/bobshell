eb67bfdf348add6be1056a493e9e32d18a635ced
Alexander Legeyda <alex@legeyda.com>
2025-03-08 20:45:38 +0300

    refactor redirect io

--- redirect/io.sh
+++ redirect/io.sh
@@ -5,8 +5,7 @@ shelduck import ./output.sh
 # fun: bobshell_redirect INPUT OUTPUT COMMAND [ARGS...]
 bobshell_redirect_io() {
 	_bobshell_redirect_io__src="$1"
-	_bobshell_redirect_io__dest="$2"
-	shift 2
-	bobshell_redirect_input "$_bobshell_redirect_io__src" bobshell_redirect_output "$_bobshell_redirect_io__dest" "$@"
-	unset _bobshell_redirect_io__src _bobshell_redirect_io__dest
+	shift
+	bobshell_redirect_input "$_bobshell_redirect_io__src" bobshell_redirect_output "$@"
+	unset _bobshell_redirect_io__src
 }
