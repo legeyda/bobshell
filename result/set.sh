cf9056aeaf8cec758c0c202eee572997fc7fda3b
Alexander Legeyda <alex@legeyda.com>
2025-03-18 11:34:30 +0300

    result_set refactoring

--- result/set.sh
+++ result/set.sh
@@ -3,5 +3,72 @@ shelduck import ../array/set.sh
 
 # fun: bobshell_result_set true
 bobshell_result_set() {
-	bobshell_array_set bobshell_result "$@"
+	if   [ 0 = "$#" ]; then
+		bobshell_result_size=0
+	elif [ 1 = "$#" ]; then
+		bobshell_result_size=1
+		bobshell_result_1="$1"
+	elif [ 2 = "$#" ]; then
+		bobshell_result_size=2
+		bobshell_result_1="$1"
+		bobshell_result_2="$2"
+	elif [ 3 = "$#" ]; then
+		bobshell_result_size=3
+		bobshell_result_1="$1"
+		bobshell_result_2="$2"
+		bobshell_result_3="$3"
+	elif [ 4 = "$#" ]; then
+		bobshell_result_size=4
+		bobshell_result_1="$1"
+		bobshell_result_2="$2"
+		bobshell_result_3="$3"
+		bobshell_result_4="$4"
+	elif [ 5 = "$#" ]; then
+		bobshell_result_size=5
+		bobshell_result_1="$1"
+		bobshell_result_2="$2"
+		bobshell_result_3="$3"
+		bobshell_result_4="$4"
+		bobshell_result_5="$5"
+	elif [ 6 = "$#" ]; then
+		bobshell_result_size=6
+		bobshell_result_1="$1"
+		bobshell_result_2="$2"
+		bobshell_result_3="$3"
+		bobshell_result_4="$4"
+		bobshell_result_5="$5"
+		bobshell_result_6="$6"
+	elif [ 7 = "$#" ]; then
+		bobshell_result_size=7
+		bobshell_result_1="$1"
+		bobshell_result_2="$2"
+		bobshell_result_3="$3"
+		bobshell_result_4="$4"
+		bobshell_result_5="$5"
+		bobshell_result_6="$6"
+		bobshell_result_7="$7"
+	elif [ 8 = "$#" ]; then
+		bobshell_result_size=8
+		bobshell_result_1="$1"
+		bobshell_result_2="$2"
+		bobshell_result_3="$3"
+		bobshell_result_4="$4"
+		bobshell_result_5="$5"
+		bobshell_result_6="$6"
+		bobshell_result_7="$7"
+		bobshell_result_7="$8"
+	elif [ 9 = "$#" ]; then
+		bobshell_result_size=9
+		bobshell_result_1="$1"
+		bobshell_result_2="$2"
+		bobshell_result_3="$3"
+		bobshell_result_4="$4"
+		bobshell_result_5="$5"
+		bobshell_result_6="$6"
+		bobshell_result_7="$7"
+		bobshell_result_8="$8"
+		bobshell_result_9="$9"
+	else
+		bobshell_array_set bobshell_result "$@"
+	fi
 }
--- result/test_set.sh
+++ result/test_set.sh
@@ -5,8 +5,44 @@ shelduck import ../assert.sh
 shelduck import ./set.sh
 
 
-test_set() {
+test_empty() {
 	bobshell_result_set
 	assert_equals 0 "$bobshell_result_size"
 	assert_unset bobshell_result_1
+}
+
+test_one() {
+	bobshell_result_set one
+	assert_equals 1 "$bobshell_result_size"
+	assert_equals one "$bobshell_result_1"
+	assert_unset bobshell_result_2
+}
+
+test_nine() {
+	bobshell_result_set 1 2 3 4 5 6 7 8 9
+	assert_equals 9 "$bobshell_result_size"
+	assert_equals 1 "$bobshell_result_1"
+	assert_equals 2 "$bobshell_result_2"
+	assert_equals 3 "$bobshell_result_3"
+	assert_equals 4 "$bobshell_result_4"
+	assert_equals 5 "$bobshell_result_5"
+	assert_equals 6 "$bobshell_result_6"
+	assert_equals 7 "$bobshell_result_7"
+	assert_equals 8 "$bobshell_result_8"
+	assert_equals 9 "$bobshell_result_9"
+}
+
+test_ten() {
+	bobshell_result_set 1 2 3 4 5 6 7 8 9 10
+	assert_equals 10 "$bobshell_result_size"
+	assert_equals 1 "$bobshell_result_1"
+	assert_equals 2 "$bobshell_result_2"
+	assert_equals 3 "$bobshell_result_3"
+	assert_equals 4 "$bobshell_result_4"
+	assert_equals 5 "$bobshell_result_5"
+	assert_equals 6 "$bobshell_result_6"
+	assert_equals 7 "$bobshell_result_7"
+	assert_equals 8 "$bobshell_result_8"
+	assert_equals 9 "$bobshell_result_9"
+	assert_equals 10 "$bobshell_result_10"
 }
