da9ee760a1aba9e5fd55d396766180a563f44542
Alexander Legeyda <alex@legeyda.com>
2025-03-18 12:03:20 +0300

    fix array_size

--- array/size.sh
+++ array/size.sh
@@ -1,8 +1,13 @@
 
 shelduck import ../base.sh
 shelduck import ./assert_isset.sh
+shelduck import ../resource/copy.sh
 
+# fun: bobshell_array_size myarr
+#      bobshell_result_assert _get_my_size
 bobshell_array_size() {
 	bobshell_array_assert_isset "$1"
-	bobshell_getvar "${1}_size" 0
+	bobshell_result_size=2
+	bobshell_result_1=true
+	bobshell_resource_copy_var_to_var "${1}_size" bobshell_result_2
 }
