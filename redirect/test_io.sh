c236d9baf9997957685ddbd27d38d6fc9b6e7605
Alexander Legeyda <alex@legeyda.com>
2025-03-20 13:07:14 +0300

    test redirect_io

--- redirect/test_io.sh
+++ redirect/test_io.sh
@@ -6,4 +6,10 @@ test_io() {
 	mkdir -p target
 	bobshell_redirect_io val:123 file:target/test_io.txt cat
 	assert_equals 123 $(cat target/test_io.txt)
+
+
+	unset x
+	bobshell_redirect_io file:target/test_io.txt var:x cat 
+	assert_equals 123 "$x"
 }
+
