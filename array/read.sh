7e3ad613f03b7faa4eefb736886e09e2df2f6430
Alexander Legeyda <alex@legeyda.com>
2025-03-27 19:50:12 +0300

    array_read add var argument

--- array/read.sh
+++ array/read.sh
@@ -6,8 +6,7 @@ shelduck import ../resource/copy.sh
 shelduck import ../result/assert.sh
 shelduck import ../result/set.sh
 
-# fun: bobshell_array_read ARRAYNAME ONEBASEDINDEX
-# todo rename to bobshell_array_get
+# fun: bobshell_array_read ARRAYNAME ONEBASEDINDEX [VARNAME]
 bobshell_array_read() {
 	if ! [ 0 -lt "$2" ]; then
 		bobshell_die "bobshell_array_read: wrong index $2 < 1"
@@ -21,6 +20,9 @@ bobshell_array_read() {
 	unset _bobshell_array_read__size
 
 	_bobshell_array_read__result=$(bobshell_getvar "$1_$2")
+	if bobshell_isset_3 "$@"; then
+		bobshell_putvar "$3" "$_bobshell_array_read__result"
+	fi
 	bobshell_result_set true "$_bobshell_array_read__result"
 	unset _bobshell_array_read__result
 }
