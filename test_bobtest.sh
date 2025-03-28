fcf27a9bd5fd17b2dce70b45dd05e8d28168ae90
Alexander Legeyda <alex@legeyda.com>
2025-03-28 19:55:38 +0300

    fix test

--- test_bobtest.sh
+++ test_bobtest.sh
@@ -6,23 +6,22 @@ shelduck import ./bobtest.sh
 
 
 test_e() {
+	: "${BOBSHELL_BUILD_ROOT:=./target}"
 	bobtest_script=$(realpath ./bobtest)
 
-	rm -rf target/test_bobtest
-	mkdir -p target/test_bobtest
-	cd target/test_bobtest
+	mkdir -p "$BOBSHELL_BUILD_ROOT/test_bobtest"
 	echo '
 	test_test() {
 		false
 		echo "OH""NO""SHOULD""NOT""PRINT""THIS"
 		bobshell_die exit test_test
 	}
-' > test_test.sh
-
+' > "$BOBSHELL_BUILD_ROOT/test_bobtest/test_test.sh"
+	
 	# https://github.com/legeyda/bobshell/actions/runs/14133698340/job/39600202215#step:3:33
 	# reset: terminal attributes: No such device or address
 	reset || true
 
-	sh -c "'$bobtest_script' test_test.sh:test_test"
+	sh -c ". '$BOBSHELL_BUILD_ROOT/shelduck.sh'; shelduck import ./bobtest.sh; bobtest '$BOBSHELL_BUILD_ROOT/test_bobtest/test_test.sh:test_test' || true"
 }
 
