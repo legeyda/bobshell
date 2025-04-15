ba4bd9aa5cf232751d70c1a135f0494e2de30dfc
Alexander Legeyda <alex@legeyda.com>
2025-04-15 22:23:49 +0300

    mktemp

--- misc/mktemp.sh
+++ misc/mktemp.sh
@@ -1,41 +1,65 @@
 
 shelduck import ../base.sh
-shelduck import ./random.sh
+shelduck import ../random/int.sh
 shelduck import ../result/set.sh
+shelduck import ../result/assert.sh
+shelduck import ../app.sh
+shelduck import ../event/listen.sh
+
 
 # see https://unix.stackexchange.com/questions/614808/why-is-there-no-mktemp-command-in-posix
 bobshell_mktemp() {
 
 	# parse cli
 	_bobshell_mktemp__dir=false
-	_bobshell_mktemp__root="${TMPDIR:-/tmp}"
 	while bobshell_isset_1 "$@"; do
 		case "$1" in
 			(-d|--directory)
 				shift
 				_bobshell_mktemp__dir=true
 				;;
+			(-*)
+				bobshell_die "$1: unexpected option"
 			(*)
-				bobshell_die "bobshell_mktemp: unexpected argument: $1"
+				break
 				;;
 		esac
 	done
 
+	# todo autoclean on exit
+	_bobshell_mktemp__root="${TMPDIR:-/tmp}/${bobshell_app_name}"
+	mkdir -p "$_bobshell_mktemp__root"
+
+
 	# generate random name
 	while true; do
-		_bobshell_mktemp__random=$(bobshell_random)
+		bobshell_random_int
+		bobshell_result_assert
+		_bobshell_mktemp__random="$bobshell_result_2"
+
+		bobshell_random_int
+		bobshell_result_assert
+		_bobshell_mktemp__random="$_bobshell_mktemp__random$bobshell_result_2"
+
+		bobshell_random_int
+		bobshell_result_assert
+		_bobshell_mktemp__random="$_bobshell_mktemp__random$bobshell_result_2"
+
 		_bobshell_mktemp__result="$_bobshell_mktemp__root/$_bobshell_mktemp__random"
 		if [ ! -e "$_bobshell_mktemp__result" ]; then
+			unset _bobshell_mktemp__random
 			break
 		fi
 	done
 
 	# create 
-	if [ true = _bobshell_mktemp__dir ]; then
+	if [ true = $_bobshell_mktemp__dir ]; then
 		mkdir "$_bobshell_mktemp__result"
 	else
 		touch "$_bobshell_mktemp__result"
 	fi
+	unset _bobshell_mktemp__dir
 	
 	bobshell_result_set true "$_bobshell_mktemp__result"
+	unset _bobshell_mktemp__result
 }
--- misc/test_mktemp.sh
+++ misc/test_mktemp.sh
@@ -7,15 +7,35 @@ shelduck import ./mktemp.sh
 shelduck import ../result/check.sh
 
 
-mkcd() {
-	mkdir -p "$1"
-	cd "$1"
+
+test_mktemp() {
+
+	bobshell_mktemp
+	bobshell_result_assert tmp1
+	assert_file_exists "$tmp1"
+
+	bobshell_mktemp
+	bobshell_result_assert tmp2
+	assert_file_exists "$tmp2"
+
+	assert_not_equals "$tmp1" "$tmp2"
+
 }
 
+test_mktemp_dir() {
 
-test_mkdir() {
-	bobshell_mktemp
-	assert_ok bobshell_result_check _dir
-	assert_file_exists "$_dir"
-	
+	bobshell_mktemp -d
+	bobshell_result_assert tmp1
+	assert_ok test -d "$tmp1"
+
+	bobshell_mktemp -d
+	bobshell_result_assert tmp2
+	assert_ok test -d "$tmp2"
+
+	assert_not_equals "$tmp1" "$tmp2"
+
+
+
+
+
 }
