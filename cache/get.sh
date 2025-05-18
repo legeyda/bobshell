51f8b501990c61a4db9e709f2a1fa199f5084483
Alexander Legeyda <alex@legeyda.com>
2025-05-19 00:04:22 +0300

    cache fix

--- cache/get.sh
+++ cache/get.sh
@@ -7,10 +7,19 @@ shelduck import ../result/check.sh
 shelduck import ../map/get.sh
 shelduck import ../map/put.sh
 
+shelduck import ../cli/default.sh
 shelduck import ../cli/param.sh
 shelduck import ../cli/parse.sh
 
+bobshell_cli_default bobshell_cache_get_cli _bobshell_cache_get__ttl none
 bobshell_cli_param   bobshell_cache_get_cli _bobshell_cache_get__ttl t ttl
+
+# fun: bobshell_shift_exec SHIFTNUM IGNORED ... COMMAND [ARGS...]
+bobshell_shift_exec() {
+	shift "$1"
+	shift
+	"$@"
+}
 
 # fun: bobshell_cache_get KEY LOADERCMD [ARGS ...]
 bobshell_cache_get() {
@@ -30,14 +39,14 @@ bobshell_cache_get() {
 		fi
 	fi
 
-
-
-	"$@"
+	set "$_bobshell_cache_get__key" "$_bobshell_cache_get__ttl" "$@" # save local state before recursive call
+	bobshell_shift_exec 2 "$@"
 	bobshell_result_read _bobshell_cache_get__value
-	bobshell_map_put bobshell_cache_data   "$_bobshell_cache_get__key" "$_bobshell_cache_get__value"
+	_bobshell_cache_get__key="$1"
+	_bobshell_cache_get__ttl="$2"
+	bobshell_map_put bobshell_cache_data "$_bobshell_cache_get__key" "$_bobshell_cache_get__value"
 
-
-	if bobshell_isset _bobshell_cache_get__ttl; then
+	if [ none != "$_bobshell_cache_get__ttl" ]; then
 		_bobshell_cache_get__deadline=$(date +%s)
 		_bobshell_cache_get__deadline=$(( _bobshell_cache_get__deadline + _bobshell_cache_get__ttl ))
 		unset _bobshell_cache_get__ttl
