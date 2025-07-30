f873e8d22fe6d6c6abf1f1495da3cfb340893093
Alexander Legeyda <alex@legeyda.com>
2025-07-30 11:19:04 +0300

    BOBTEST_REDIRECT

--- bobtest.sh
+++ bobtest.sh
@@ -43,6 +43,9 @@ bobtest_file() {
 		bobtest_file_function "$_bobtest_file_real_path" "$_bobtest_file__function"
 	else
 		all__bobtest_file__functions=$(sed -n --regexp-extended 's/^( *function)? *(test_\w+) *\( *\) *\{ *$/\2/pg' "$_bobtest_file_real_path")
+		if [ -z "$all__bobtest_file__functions" ]; then
+			echo "no test_* function found in file $_bobtest_file"
+		fi
 		for _bobtest_file__function in $all__bobtest_file__functions; do
 			bobtest_file_function "$_bobtest_file_real_path" "$_bobtest_file__function"
 		done
@@ -56,14 +59,18 @@ bobtest_file_function() {
 
 	printf '  function %s... ' "$2"
 
-	bobtest_run "$@"
+	bobtest_file_function_run "$@"
 	if ! bobshell_result_check; then
 		printf 'failure\n\n'
-		printf '\n\nSTDOUT WAS:\n%s\n'
-		cat "$bobshell_stdout_file"
-		
-		printf '\n\nSTDERR WAS:\n%s\n'
-		cat "$bobshell_stderr_file"
+		if [ true = "${BOBTEST_REDIRECT:-true}" ]; then
+			printf '\n\nSTDOUT WAS:\n%s\n'
+			cat "$bobshell_stdout_file"
+			
+			printf '\n\nSTDERR WAS:\n%s\n'
+			cat "$bobshell_stderr_file"
+		else
+			echo "stdin and stdout see above"
+		fi
 		
 		return 1
 	fi
@@ -76,16 +83,15 @@ bobtest_output_flag=6b2e8ddb5198464ea952c131e17bfede9966f038c958468e8ec5108d2ed7
 
 # fun: bobtest_run FILE FUNCTIION
 # env: 
-bobtest_run() {
-	: "${BOBTEST_RUN:=bobtest_shelduck_run}"
+bobtest_file_function_run() {
+	: "${BOBTEST_RUN:=bobtest_run}"
 
-	_bobtest_run__script='( set -eu; printf %s "$(set -eux; "$BOBTEST_RUN" "$@"; printf %s "$bobtest_output_flag")" )'
+	_bobtest_file_function_run__script='( set -eu; printf %s "$(set -eux; "$BOBTEST_RUN" "$@"; printf %s "$bobtest_output_flag")" )'
 	if [ true = "${BOBTEST_REDIRECT:-true}" ]; then
-		_bobtest_run__script="$_bobtest_run__script"' > "$bobshell_stdout_file" 2> "$bobshell_stderr_file"'
+		_bobtest_file_function_run__script="$_bobtest_file_function_run__script"' > "$bobshell_stdout_file" 2> "$bobshell_stderr_file"'
 	fi
-	eval "$_bobtest_run__script"
-	unset _bobtest_run__script
-
+	eval "$_bobtest_file_function_run__script"
+	unset _bobtest_file_function_run__script
 	 
 	
 	if grep -q "$bobtest_output_flag" "$bobshell_stdout_file"; then
@@ -97,15 +103,25 @@ bobtest_run() {
 	unset _bobtest_run_output
 }
 
-bobtest_shelduck_run() {
+bobtest_run() {
 	export BOBTEST_FILE="$1"
 	export BOBTEST_FUNCTION="$2"
 
-	_bobtest_shelduck_run=$(shelduck resolve "file://$1")
+	_bobtest_run__script=$(shelduck resolve "file://$1")
+	_bobtest_run__script="$_bobtest_run__script
 
-	${BOBTEST_SHELL_COMMAND:-sh -euc} "$_bobtest_shelduck_run
-	
 set -x
 $2"
-	unset _bobtest_shelduck_run
+
+	if [ "${#_bobtest_run__script}" -lt 100000 ]; then
+		${BOBTEST_SHELL_COMMAND:-sh -eu} -c "$_bobtest_run__script"
+	else
+		_bobtest_run__file=$(mktemp)
+		printf %s "$_bobtest_run__script" > "$_bobtest_run__file"
+		${BOBTEST_SHELL_COMMAND:-sh -eu} "$_bobtest_run__file"
+		rm -r "$_bobtest_run__file"
+		unset _bobtest_run__file
+	fi
+	
+	unset _bobtest_run__script
 }
