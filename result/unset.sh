ba8ed720a27c399c5d21a03a60cda234820bb891
Alexander Legeyda <alex@legeyda.com>
2026-08-24 21:20:30 +0300

    result_unset refactoring

--- result/unset.sh
+++ result/unset.sh
@@ -1,18 +1,22 @@
 
 bobshell_result_unset() {
-	if   [ 0 = "${bobshell_result_size:-0}" ]; then
-		unset bobshell_result_size
-	elif [ 1 = "$bobshell_result_size" ]; then
-		unset bobshell_result_size bobshell_result_1
-	elif [ 2 = "$bobshell_result_size" ]; then
-		unset bobshell_result_size bobshell_result_1 bobshell_result_2
-	elif [ 3 = "$bobshell_result_size" ]; then
-		unset bobshell_result_size bobshell_result_1 bobshell_result_2 bobshell_result_3
-	else
-		while [ 0 -lt "$bobshell_result_size" ]; do
-			unset "bobshell_result_$bobshell_result_size"
-			bobshell_result_size=$(( bobshell_result_size - 1 ))
+
+
+	if [ "${bobshell_result_size:-0}" -gt 0 ]; then
+		unset bobshell_result_1
+		_bobshell_result_unset__i=2
+		while [ "$_bobshell_result_unset__i" -lt "$bobshell_result_size" ]; do
+			unset bobshell_result_$_bobshell_result_unset__i
+			_bobshell_result_unset__i=$(( _bobshell_result_unset__i + 1 ))
 		done
-		unset bobshell_result_size
-	fi	
+	else
+		_bobshell_result_unset__i=1	
+	fi
+	unset bobshell_result_size
+
+	while bobshell_isset bobshell_result_"$_bobshell_result_unset__i"; do
+		unset bobshell_result_"$_bobshell_result_unset__i"
+		_bobshell_result_unset__i=$(( _bobshell_result_unset__i + 1 ))
+	done
+	unset _bobshell_result_unset__i
 }
