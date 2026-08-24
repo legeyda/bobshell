56384822ae2bbe44bbedac9d7cf8d11872c8f30a
Alexander Legeyda <alex@legeyda.com>
2026-08-24 12:38:08 +0300

    result_read refactoring

--- result/read.sh
+++ result/read.sh
@@ -2,42 +2,18 @@
 shelduck import ../base.sh
 shelduck import ../resource/copy.sh
 
-# fun: bobshell_result_get VAR ...
+# fun: bobshell_result_read VAR ...
 bobshell_result_read() {
-
-	if [ "$#" -gt "${bobshell_result_size:-0}" ]; then
-		bobshell_die "number of resulting variables ($#) is greater than number of available values (${bobshell_result_size:-0}) "
+	if ! [ "$#" -le "${bobshell_result_size:-0}" ]; then
+		printf 'number of resulting variables (%s) is greater than number of available values (%d)\s' "$#" "${bobshell_result_size:-0}" >&2
+		return 1
 	fi
 
-	if   [ 1 = "$#" ]; then
-		bobshell_resource_copy_var_to_var bobshell_result_1 "$1"
-	elif [ 2 = "$#" ]; then
-		bobshell_resource_copy_var_to_var bobshell_result_1 "$1"
-		bobshell_resource_copy_var_to_var bobshell_result_2 "$2"
-	elif [ 3 = "$#" ]; then
-		bobshell_resource_copy_var_to_var bobshell_result_1 "$1"
-		bobshell_resource_copy_var_to_var bobshell_result_2 "$2"
-		bobshell_resource_copy_var_to_var bobshell_result_3 "$3"
-	elif [ 4 = "$#" ]; then
-		bobshell_resource_copy_var_to_var bobshell_result_1 "$1"
-		bobshell_resource_copy_var_to_var bobshell_result_2 "$2"
-		bobshell_resource_copy_var_to_var bobshell_result_3 "$3"
-		bobshell_resource_copy_var_to_var bobshell_result_4 "$4"
-	elif [ 5 = "$#" ]; then
-		bobshell_resource_copy_var_to_var bobshell_result_1 "$1"
-		bobshell_resource_copy_var_to_var bobshell_result_2 "$2"
-		bobshell_resource_copy_var_to_var bobshell_result_3 "$3"
-		bobshell_resource_copy_var_to_var bobshell_result_4 "$4"
-		bobshell_resource_copy_var_to_var bobshell_result_5 "$5"
-	else
-		for _bobshell_result_read__i in $(seq "$#"); do
-			bobshell_resource_copy_var_to_var "bobshell_result_$_bobshell_result_read__i" "$1"
-			shift
-			if ! bobshell_isset_1 "$@"; then
-				break
-			fi
-		done
-		unset _bobshell_result_read__i
-	fi
-
+	for _bobshell_result_read__i in $(seq "$#"); do
+	    if [ "$1" ] && [ - != "$1" ]; then
+		    bobshell_resource_copy_var_to_var "bobshell_result_$_bobshell_result_read__i" "$1"
+        fi
+		shift
+	done
+	unset _bobshell_result_read__i
 }
