f6941f81c94f825b76e8d865be650d4fbfbb95c5
Alexander Legeyda <alex@legeyda.com>
2026-08-24 19:38:56 +0300

    event_var_listen fix style

--- event/var/listen.sh
+++ event/var/listen.sh
@@ -8,11 +8,11 @@ bobshell_event_var_listen() {
 	_bobshell_event_var_listen__moment=after
 	while bobshell_isset_1 "$@"; do
 		case "$1" in
-			(-a|--after) 
+			(-a|--after)
 				_bobshell_event_var_listen__moment=after
 				shift 1
 				;;
-			(-b|--before) 
+			(-b|--before)
 				_bobshell_event_var_listen__moment=before
 				shift 1
 				;;
@@ -32,6 +32,6 @@ bobshell_event_var_listen() {
  	else
 		bobshell_die "unsupported moment value $_bobshell_event_var_listen__moment"
 	fi
-	
+
 	unset _bobshell_event_var_listen__moment _bobshell_event_var_listen__var
 }
